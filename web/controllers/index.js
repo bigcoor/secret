942a49e36bc0c999a9a6d4737a433d1c580bc1f6
meepo <homheihdu@gmail.com>
2014-08-14 23:36:43 +0800

    [secret] fix prod mongodb url

--- web/controllers/index.js
+++ web/controllers/index.js
@@ -8,14 +8,9 @@ exports.notified = function(req, res) {
   var email = req.body.userEmail;
   if (email) {
     User.create({ email : email}, function(err, user) {
-      if(!err) {
-        res.json({
-          error : null
-        , data  : null });
-      }
-    res.json({
-          error : err
-        , data  : null });
+      res.json({
+        error : err
+      , data  : user });
     });
   }
 };
