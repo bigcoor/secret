68fd1b48de39874c19d8857ab487bec52a18a32c
meepo <homheihdu@gmail.com>
2014-11-30 11:17:36 +0800

    disable mongo

--- web/controllers/index.js
+++ web/controllers/index.js
@@ -1,16 +1,3 @@
-var User = require('../../models/user');
-
 exports.index = function(req, res) {
   res.render('index');
-};
-
-exports.notified = function(req, res) {
-  var email = req.body.userEmail;
-  if (email) {
-    User.create({ email : email}, function(err, user) {
-      res.json({
-        error : err
-      , data  : user });
-    });
-  }
 };
--- web/routes.js
+++ web/routes.js
@@ -2,5 +2,4 @@ var index = require('./controllers/index');
 
 module.exports = function(app) {
   app.get('/', index.index);
-  app.post('/notified.json', index.notified);
 };
